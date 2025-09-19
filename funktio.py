0978504faa113b10bff3d65f81c960da6d3a7a2f
antonoo <antonoo@metropolia.fi>
2025-09-19 14:27:30 +0300

    muutos

--- funktio.py
+++ funktio.py
@@ -1,30 +1,13 @@
-import random
-import story
 from geopy import distance
-
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    database='demogame',
-    user='kayttaja',
-    password='salasana',
-    autocommit=True
-)
+connection = mysql.connector.connect(
+    port=3306, #oletusarvo ei pakollinen.
+    host="127.0.0.1", #oletusarvo ei pakollinen.
+    database = 'flight_game', 
+    user='root',
+    password='Rekolammas123',
+    autocommit=True)
 
 
-# FUNCTIONS
 
-# select 30 airports for the game
-def get_airports():
-    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
-FROM airport
-WHERE continent = 'EU' 
-AND type='large_airport'
-ORDER by RAND()
-LIMIT 30;"""
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
