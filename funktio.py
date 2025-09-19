d78674487cf8450b5f7765c666bb125a1fb5c742
eeroh <eero.helimaki@metropolia.fi>
2025-09-19 14:54:45 +0300

    yeayea

--- funktio.py
+++ funktio.py
@@ -4,9 +4,9 @@ import mysql.connector
 connection = mysql.connector.connect(
     port=3306, #oletusarvo ei pakollinen.
     host="127.0.0.1", #oletusarvo ei pakollinen.
-    database = 'flight_game', 
-    user='root',
-    password='Rekolammas123',
+    database = 'demogame',
+    user='eero',
+    password='yeah123',
     autocommit=True)
 
 def airports():
