f7c5da554a9b42f36111542fc5825074b1696457
antonoo <antonoo@metropolia.fi>
2025-09-22 09:40:03 +0300

    Käyttäjänimen ja salasanan päivitys

--- funktio.py
+++ funktio.py
@@ -4,9 +4,9 @@ import mysql.connector
 connection = mysql.connector.connect(
     port=3306, #oletusarvo ei pakollinen.
     host="127.0.0.1", #oletusarvo ei pakollinen.
-    database = 'flight_game', 
-    user='root',
-    password='Rekolammas123',
+    database = 'projektipeli', 
+    user='projekti',
+    password='sala',
     autocommit=True)
 
 def airports():
