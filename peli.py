e7968a578921446a71599b21814e8a39aba69069
eeroh <eero.helimaki@metropolia.fi>
2025-09-22 11:55:18 +0300

    yeayea

--- peli.py
+++ peli.py
@@ -1,18 +1,24 @@
 
-
+current_airport = start_airport
 
 game_running = True
 while game_running:
     # get current airport info
     airport = airport_data(current_airport)
-    # show game status
-    print(f'Olet nyt {airport['name']}.')
+
+    # todo kerrotaan kuinka lähellä on maalia ja kuinka lähellä npc on
+    print(f'Olet nyt lentokentällä:  {airport['name']}.')
     print(f'sinulla on {player_range:.0f}kilometriä rangea.')
     # kysytään haluuako ladata, heittää noppaa tai lentää.
-    #kerrotaan kuinka lähellä on maalia ja kuinka lähellä npc on
+    do = input('haluatko ladata (lataa), heittää noppaa(heita) tai lentää(lenna)')
+    if do == 'lataa':
 
-    destination = input('Enter destination icao: ') #liikutaan seuraavaan pisteeseen ja päivitetään lokaatio
-    selected_distance = calculate_distance(current_airport, destination)
-    player_range -= selected_distance
-    update_location(destination, player_range)
-    current_airport = destination
+    elif do == 'heita':
+        throw_dice()
+    elif do == 'lenna':
+        # todo anna lento vaihtoehdot
+        destination = input('Enter destination icao: ') #liikutaan seuraavaan pisteeseen ja päivitetään lokaatio
+        selected_distance = calculate_distance(current_airport, destination)
+        player_range -= selected_distance
+        update_location(destination, player_range)
+        current_airport = destination
