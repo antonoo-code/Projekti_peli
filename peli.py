bd6203594fdfa7706c731e61352cd144eb94cd10
eeroh <eero.helimaki@metropolia.fi>
2025-09-30 10:20:09 +0300

    yeayea

--- peli.py
+++ peli.py
@@ -244,7 +244,7 @@ while game_running:
             while lenna:
                 player_flight_options = player_airport_range_calc(current_airport, all_airports, player_range)
                 for i in player_flight_options: #Anton
-                    print(i)
+                    print(f'{i[0]}, {BLUE}{i[1]}{RESET}, {i[2]}')
                 print(f"{GREEN} Maali on: {end_airport['name']} ({goal_airport}){RESET}")
                 destination = input(f'Syötä lentokentän {BLUE}ICAO{RESET} koodi: ') #liikutaan seuraavaan pisteeseen ja päivitetään lokaatio
                 destination = str.upper(destination)
