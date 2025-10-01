dffbba0942ea188833e60bf2ee1f6b5f82ed0be3
rohan <Rohan.Thapamagar@metropolia.fi>
2025-10-01 09:28:56 +0300

    hienosäätöä

--- peli.py
+++ peli.py
@@ -199,7 +199,7 @@ while game_running:
             print(f'{GREEN}Sinun sijaintisi on: {RESET} {get_airport_name(current_airport)} matkaa maaliin on: {calculate_distance(current_airport, goal_airport):.0f} kilometriä, sekä sinulla on rangea jäljellä {player_range:.0f} kilometriä.')
             print(f'{YELLOW}Möttösen sijainti on:{RESET} {get_airport_name(npc_current_airport)} ja matkaa maaliin on: {calculate_distance(npc_current_airport, goal_airport):.0f} kilometriä.') #Anton
         if calculate_distance(npc_current_airport, goal_airport)+200 < calculate_distance(current_airport, goal_airport):
-            print(f' {YELLOW}Möttönen lähetti sinulle viestin: Missä kaveri hinaa XDD {RESET}')
+            print(f'{YELLOW}Möttönen lähetti sinulle viestin: Missä kaveri hinaa XDD {RESET}')
         player_flight_options = player_airport_range_calc(current_airport, all_airports, player_range)
         if player_flight_options == []:
             print('Sinulla ei ole rangea lentää minnekkään.')
