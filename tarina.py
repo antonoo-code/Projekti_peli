df8c2cfdfa029e6d64140f2ad44c504fab67b075
nickenyberg <nicken@metropolia.fi>
2025-10-01 10:53:44 +0300

    asdfasdfaftarina

--- tarina.py
+++ tarina.py
@@ -2,7 +2,7 @@ import textwrap
 
 story = '''Olet intensiivisessä lentokisassa Möttöstä vastaan. 
 
-Tavoitteesi on voittaa mahdollisimman lyhyessä ajassa. Kisa ei ole kuitenkaan yksinkertainen, 
+Tavoitteesi on päästä maaliin ennen Möttöstä. Kisa ei ole kuitenkaan yksinkertainen, 
 
 Islannin, Espanjan ja Portugalin verisen sodan syystä niiden maiden lentokentät eivät ole käytettävissä.
 
