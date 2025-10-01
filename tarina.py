54016956832c8e380663c56f622cedfb0b14e963
rohan <Rohan.Thapamagar@metropolia.fi>
2025-10-01 10:56:16 +0300

    hienosäätöä

--- tarina.py
+++ tarina.py
@@ -1,6 +1,7 @@
 import textwrap
 GOLD = "\033[38;5;220m"
 ITALIC = "\033[3m"
+RESET = "\033[0m"
 
 story = f'''{GOLD}{ITALIC}Olet intensiivisessä lentokisassa Möttöstä vastaan. 
 
@@ -8,7 +9,7 @@ Tavoitteesi on päästä maaliin ennen Möttöstä. Kisa ei ole kuitenkaan yksin
 
 Islannin, Espanjan ja Portugalin verisen sodan syystä niiden maiden lentokentät eivät ole käytettävissä.
 
-Lataa lentokone tai heitä noppaa saadaksesi yllätyksiä lentokentillä. '''
+Lataa lentokone tai heitä noppaa saadaksesi yllätyksiä lentokentillä.{RESET}'''
 
 wrapper = textwrap.TextWrapper(width=60, break_long_words=False, replace_whitespace=False)
 
