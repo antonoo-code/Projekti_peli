23d08aeb22f0301c5c17a1ccc93168a4f38e6ad5
rohan <Rohan.Thapamagar@metropolia.fi>
2025-10-06 10:34:20 +0300

    hienosäätöä

--- peli.py
+++ peli.py
@@ -307,6 +307,7 @@ while game_running:
                 for i in player_flight_options: #Anton
                     print(f'{i[0]:<42}, {BLUE}{i[1]}{RESET}, {i[2]}km')
                 print(f"{GREEN}Maali on: {end_airport['name']} ({goal_airport}){RESET}")
+                print("Jos haluat palata takaisin, paina Enter")
                 destination = input(f'Syötä lentokentän {BLUE}ICAO{RESET} koodi: ') #liikutaan seuraavaan pisteeseen ja päivitetään lokaatio
                 print('')
                 if destination == '':
